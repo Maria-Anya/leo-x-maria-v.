d2422966625208de81c71815ad44fe3db7412f09
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-10-10 12:11:15 -0300

    lll

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 function criaCartao(categoria, pergunta, respota){
    let container = document.getElementById('container');
    let cartao = document.createElement('cartao');
-   
+   cartao.className = 'cartao';
+   cartao.innerHTML = `
+   <div class="conteudo-cartao">
+    <h3>categoria</h3>
+    <div class="pergunta-cartao">
+        <p>pergunta</p>
+    </div>
+    <div class="resposta-cartao">
+        <p>respota</p>
+    </div>
+</div>
+   `
+
+   container.appendChild(cartao);
 }

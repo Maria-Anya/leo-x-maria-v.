4c0d3add186106d63d70116fa8746b786e89bf1e
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-10-10 12:21:35 -0300

    g

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function criaCartao(categoria, pergunta, respota){
    <div class="conteudo-cartao">
     <h3>${categoria}</h3>
     <div class="pergunta-cartao">
-        <p>pergunta</p>
+        <p>${pergunta}</p>
     </div>
     <div class="resposta-cartao">
         <p>respota</p>

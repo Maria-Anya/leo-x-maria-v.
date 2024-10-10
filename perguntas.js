a5cdf498a8f5ca602924c251f62e997bce1cc5b7
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-10-10 11:51:05 -0300

    ggg

---
+++ perguntas.js
@@ -0,0 +1,84 @@
+criaCartao(
+    'robotica',
+    'qual e a perna negativa do led RGB',
+    'respota:a maoir entre as quatro perninhas',
+)
+
+criaCartao(
+    'robotica',
+    ' para que serve o USB',
+    ' para passar os codigos para o projeto'
+)
+
+criaCartao(
+    'matematica',
+    'tangente de 45',
+    ' 1')
+
+criaCartao('matematica',
+    ' coseno de 60',
+    ' raiz de tres sobre 2 ')
+
+criaCartao('historia',
+    ' o que o Vargas fez que influencia nossa vida ate hoje',
+    'CLT')
+
+criaCartao('historia',
+    ' qual povo religioso o Hitler odiava',
+    ' judeus')
+
+criaCartao('programaçao',
+    ' como faz para entar na pag. de digitar os codigos',
+    ' é só apertar o ponto')
+
+criaCartao('programacao',
+    ' qual e o codigo que inicia o corpo do site',
+    ' é o HTML')
+
+criaCartao('geografia',
+    ' por que as calotas polares estao deretendo',
+    ' por causa do aquecimento global')
+
+criaCartao('ingles',
+    ' como se diz voce',
+    ' é you')
+
+criaCartao('ingles',
+    ' palavra principal do primeiro condicional ',
+    ' é with')
+    
+criaCartao('biologia',
+    ' como saber se a pessoa é o negativo',
+    ' quando nenhum sangue coagula no teste')
+
+criaCartao('biologia',
+    ' o que e gene recessivo ',
+    ' é o gene mais fraco')
+
+criaCartao('educaçao financeira',
+    ' qual e a formula do juro composto',
+    ' M = J . C')
+
+criaCartao('quimica',
+    ' qual e o nome do carbono quando ele esta isolado da formula ',
+    ' metil')
+
+criaCartao('quimica',
+    ' qual elemento se estabiliza com 8 eletrons na camada de valencia',
+    ' todos menos o hidrogenio')
+
+criaCartao('quimica',
+    ' qual e a formula da agua',
+    ' h2o')
+
+criaCartao('quimica',
+    ' qual eo elemento que tem um na sua camada de valencia',
+    ' o hidrogenio')
+
+criaCartao('historia',
+    '  quem foi o presidente que teve o queremismo',
+    ' Getulio Vargas')
+
+criaCartao('Projeto de vida',
+    ' o que é empatia',
+    ' e o ato de se por no lugar dos outros')

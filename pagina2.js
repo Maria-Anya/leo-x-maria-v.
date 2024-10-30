f31fb909fdd64422cdb61f5960d325010728f2cc
Maria-Anya <134070005+Maria-Anya@users.noreply.github.com>
2024-10-30 08:15:58 -0300

    ds

---
+++ pagina2.js
@@ -0,0 +1,27 @@
+function criaCartao(categoria, pergunta, respota){
+    let container = document.getElementById('container');
+    let cartao = document.createElement('cartao');
+    cartao.className = 'cartao';
+    cartao.innerHTML = `
+    <div class="conteudo-cartao">
+     <h3>${categoria}</h3>
+     <div class="pergunta-cartao">
+         <p>${pergunta}</p>
+     </div>
+     <div class="resposta-cartao">
+         <p>${respota}</p>
+     </div>
+ </div>
+    `
+ 
+    let respostaEstaVisivel = false;
+ 
+    function viraCartao(){
+     respostaEstaVisivel = !respostaEstaVisivel;
+     cartao.classList.toggle('active',respostaEstaVisivel);
+    }
+ 
+    cartao.addEventListener('click',viraCartao);
+ 
+    container.appendChild(cartao);
+ }
